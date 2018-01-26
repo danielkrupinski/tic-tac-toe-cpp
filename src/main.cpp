c08bd8bb56296bd515a483b44eb97cffdd75a1fe
Daniel Krupiński <danielkrupinski@outlook.com>
2018-01-26 12:27:10 +0100

    Add setup() function

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,29 @@
 
 using namespace std;
 
+void setup()
+{
+	board Board;
+	player Player1;
+	Player1.player_id(1);
+	Player1.player_sign('X');
+	player Player2;
+	Player2.player_id(2);
+	Player2.player_sign('O');
+
+}
 int main()
 {
-	cout << "Tic-tac-toe" << endl;
-	board Board;
+	setup();
+	while(true)
+	{
+		Board.draw();
+		Player1.move(Board);
+		Board.check();
+		Board.draw();		
+		Player2.move(Board);
+		Board.check();
+	}
 	Board.draw();
 	getchar();
 	return 0;
