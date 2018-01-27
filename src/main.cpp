9fbe08bc6d5ebd3cf740b3ce13d67262e025fd78
Daniel Krupiński <danielkrupinski@outlook.com>
2018-01-27 17:28:09 +0100

    Add game results

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,20 @@ int main()
 {
 	string square = "0123456789";
 	char player = 'X';
-	while(true)
+	int status = -1;
+	while (status == -1)
 	{
 		move(square, player);
+		status = check(square);
 	}
+	player = player == 'X' ? 'O' : 'X';
+	draw_board(square);
+	if (status == 1)
+		cout << "\nPlayer " << player << " won!";
+	else
+		cout << "\nGame draw!";
+	cin.ignore();
+	getchar();
 	return 0;
 }
 
