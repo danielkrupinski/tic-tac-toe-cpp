e3a249587b8faaa90edbf703353751de1bbcd545
Daniel Krupiński <danielkrupinski@outlook.com>
2018-01-28 11:11:48 +0100

    Add possibility to play again

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,26 @@
 
 int main()
 {
-	string square = "0123456789";
-	char player = 'X';
-	int status = -1;
-	while (status == -1)
+	char again = 'Y';
+	while (again == 'Y' || again == 'y')
 	{
-		move(square, player);
-		status = check(square);
+		string square = "0123456789";
+		char player = 'X';
+		int status = -1;
+		while (status == -1)
+		{
+			move(square, player);
+			status = check(square);
+		}
+		player = player == 'X' ? 'O' : 'X';
+		draw_board(square);
+		if (status == 1)
+			cout << "\nPlayer " << player << " won!\n";
+		else
+			cout << "\nGame draw!\n";
+		cout << "\vPlay again? (Y)es, (N)o/(E)xit? ";
+		cin >> again;
 	}
-	player = player == 'X' ? 'O' : 'X';
-	draw_board(square);
-	if (status == 1)
-		cout << "\nPlayer " << player << " won!";
-	else
-		cout << "\nGame draw!";
-	cin.ignore();
-	getchar();
 	return 0;
 }
 
