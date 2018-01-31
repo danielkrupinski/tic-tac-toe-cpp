dee9016d6e7837b9b47e36ee56e34e4ac08efdef
Daniel Krupiński <danielkrupinski@outlook.com>
2018-01-31 16:19:04 +0100

    Source code optimizations

--- src/functions.cpp
+++ src/functions.cpp
@@ -6,7 +6,7 @@ void draw_board(const string& s)
 {
 	system("clear");
 	cout << "Tic-Tac-Toe\n\v";
-	for(int i = 1; i != 10; ++i)
+	for(int i=1; i!=10; ++i)
 	{
 		cout << ' ' << s[i] << ' ';
 		if(i % 3)
@@ -21,7 +21,7 @@ void move(string& s, char& player)
 {
 	draw_board(s);
 	cout << "\nPlayer " << player << " move: ";
-	int choice;
+	int choice {0};
 	cin >> choice;
 
 	while (choice < 1 || choice > 9 || s[choice] == 'X' || s[choice] == 'O')
@@ -39,14 +39,14 @@ void move(string& s, char& player)
 
 int check(const string& s)
 {
-	const int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
-                   	      {1,4,7},{2,5,8},{3,6,9},
-                   	      {1,5,9},{3,5,7}};
-	for (int i = 0; i != 8; ++i)
+	static const int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
+				     {1,4,7},{2,5,8},{3,6,9},
+				     {1,5,9},{3,5,7}};
+	for (int i=0; i!=8; ++i)
 		if (s[ways[i][0]] == s[ways[i][1]] && s[ways[i][1]] == s[ways[i][2]])
 			return 1;	
-	int count = 0;
-	for (int i = 1; i != 10; ++i)
+	int count {0};
+	for (int i=1; i!=10; ++i)
 		if (s[i] != 48+i)
 			++count;
 	if (count == 9)
