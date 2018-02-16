6e698a5ad80fb12e8cdc8247c0739f303aa40634
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-16 18:41:35 +0100

    Update code formatting

--- src/board.cpp
+++ src/board.cpp
@@ -21,8 +21,8 @@ void move(string& s, char& player, const int& mode, int& status)
 {
     draw(s);
     constexpr int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
-    {1,4,7},{2,5,8},{3,6,9},
-    {1,5,9},{3,5,7}};
+                              {1,4,7},{2,5,8},{3,6,9},
+                              {1,5,9},{3,5,7}};
     if (mode == 1 || player == 'X')
     {
         cout << "\nPlayer " << player << " moves: ";
@@ -42,54 +42,53 @@ void move(string& s, char& player, const int& mode, int& status)
     }
     else
     {
-
         bool x {false};
         for (int i=0; i!=8; ++i)
-        if (s[ways[i][0]] == 'X' && s[ways[i][1]] == 'X' && s[ways[i][2]] < 58)
-        {
-            s[ways[i][2]] = player;
-            x = true;
-            break;
-        }
-        else if (s[ways[i][1]] == 'X' && s[ways[i][2]] == 'X' && s[ways[i][0]] < 58)
-        {
-            s[ways[i][0]] = player;
-            x = true;
-            break;
-        }
-        else if (s[ways[i][0]] == 'X' && s[ways[i][2]] == 'X' && s[ways[i][1]] < 58)
-        {
-            s[ways[i][1]] = player;
-            x = true;
-            break;
-        }
+            if (s[ways[i][0]] == 'X' && s[ways[i][1]] == 'X' && s[ways[i][2]] < 58)
+            {
+                s[ways[i][2]] = player;
+                x = true;
+                break;
+            }
+            else if (s[ways[i][1]] == 'X' && s[ways[i][2]] == 'X' && s[ways[i][0]] < 58)
+            {
+                s[ways[i][0]] = player;
+                x = true;
+                break;
+            }
+            else if (s[ways[i][0]] == 'X' && s[ways[i][2]] == 'X' && s[ways[i][1]] < 58)
+            {
+                s[ways[i][1]] = player;
+                x = true;
+                break;
+            }
         if (!x)
-        for (int i=1; i!=s.size(); i+=2)
-        if (s[i] < 58)
-        {
-            s[i] = player;
-            break;
-        }
+            for (int i=1; i!=s.size(); i+=2)
+                if (s[i] < 58)
+                {
+                    s[i] = player;
+                    break;
+                }
     }
     status = check(s);
     if (status == -1)
-    player = player == 'X' ? 'O' : 'X';
+        player = player == 'X' ? 'O' : 'X';
     else draw(s);
 }
 
 int check(const string& s)
 {
     constexpr int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
-    {1,4,7},{2,5,8},{3,6,9},
-    {1,5,9},{3,5,7}};
+                              {1,4,7},{2,5,8},{3,6,9},
+                              {1,5,9},{3,5,7}};
     for (int i=0; i!=8; ++i)
-    if (s[ways[i][0]] == s[ways[i][1]] && s[ways[i][1]] == s[ways[i][2]])
-    return 1;
+        if (s[ways[i][0]] == s[ways[i][1]] && s[ways[i][1]] == s[ways[i][2]])
+            return 1;
     int count {};
     for (int i=1; i!=10; ++i)
-    if (s[i]!=48+i)
-    ++count;
+        if (s[i]!=48+i)
+            ++count;
     if (count == 9)
-    return 0;
+        return 0;
     return -1;
 }
