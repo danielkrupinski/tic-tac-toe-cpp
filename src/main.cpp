c3a0472042d20d85fd389bd272f2b26f6495a4fb
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-16 18:48:02 +0100

    Update code formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,25 @@ using namespace std;
 
 int main()
 {
-	char again {'Y'};
-	cout << "Select mode:\n 1. Player vs player\n 2. Player vs ai ";
-	int mode {};
-	cin >> mode;
-	while (again == 'Y' || again == 'y')
-	{
-		string square {"0123456789"};
-		char player {'X'};
-		int status {-1};
-		while (status == -1)
-		{
-			move(square, player, mode, status);
-		}
-		if (status == 1)
-			cout << "\nPlayer " << player << " won!\n";
-		else
-			cout << "\nGame draw!\n";
-		cout << "\nPlay again? (Y)es, (N)o/(E)xit? ";
-		cin >> again;
-	}
-	return 0;
+    char again {'Y'};
+    cout << "Select mode:\n 1. Player vs player\n 2. Player vs ai ";
+    int mode {};
+    cin >> mode;
+    while (again == 'Y' || again == 'y')
+    {
+        string square {"0123456789"};
+        char player {'X'};
+        int status {-1};
+        while (status == -1)
+        {
+            move(square, player, mode, status);
+        }
+        if (status == 1)
+        cout << "\nPlayer " << player << " won!\n";
+        else
+        cout << "\nGame draw!\n";
+        cout << "\nPlay again? (Y)es, (N)o/(E)xit? ";
+        cin >> again;
+    }
+    return 0;
 }
-
