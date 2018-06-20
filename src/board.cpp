680f3bdd3c0a9fb31eec8b9bb1dede049c1cbd82
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-20 14:26:19 +0200

    Update formatting

--- src/board.cpp
+++ src/board.cpp
@@ -7,7 +7,7 @@ void draw(const string& s)
 {
     system("clear");
     cout << "Tic-Tac-Toe\n\n";
-    for(int i=1; i!=10; ++i)
+    for(int i = 1; i != 10; ++i)
     {
         cout << ' ' << s[i] << ' ';
         if (i % 3)
@@ -27,7 +27,7 @@ void move(string& s, char& player, const int& mode, int& status)
     if (mode == 1 || player == 'X')
     {
         cout << "\nPlayer " << player << " moves: ";
-        static int choice {};
+        static int choice;
         cin >> choice;
 
         while (choice < 1 || choice > 9 || s[choice] > 57)
@@ -43,8 +43,8 @@ void move(string& s, char& player, const int& mode, int& status)
     }
     else
     {
-        bool two_in_row {false};
-        for (int i=0; i!=8; ++i)
+        bool two_in_row{false};
+        for (int i = 0; i != 8; ++i)
             if (s[ways[i][0]] == 'X' && s[ways[i][1]] == 'X' && s[ways[i][2]] < 58)
             {
                 s[ways[i][2]] = player;
@@ -64,7 +64,7 @@ void move(string& s, char& player, const int& mode, int& status)
                 break;
             }
         if (!two_in_row)
-            for (int i=1; i!=s.size(); i+=2)
+            for (int i = 1; i != s.size(); i += 2)
                 if (s[i] < 58)
                 {
                     s[i] = player;
@@ -82,11 +82,11 @@ int check(const string& s)
     constexpr int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
                               {1,4,7},{2,5,8},{3,6,9},
                               {1,5,9},{3,5,7}};
-    for (int i=0; i!=8; ++i)
+    for (int i = 0; i != 8; ++i)
         if (s[ways[i][0]] == s[ways[i][1]] && s[ways[i][1]] == s[ways[i][2]])
             return 1;
-    bool full {true};
-    for (int i=1; i!=10; ++i)
+    bool full{true};
+    for (int i = 1; i != 10; ++i)
         if (s[i]==48+i)
             full = false;
     if (!full)
