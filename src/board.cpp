babea8dfe26c7241e1b64b994097c69e58111ae9
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-20 14:48:15 +0200

    Make constexpr int ways[][] static

--- src/board.cpp
+++ src/board.cpp
@@ -79,9 +79,9 @@ void move(string& s, char& player, const int mode, int& status)
 
 int check(const string& s)
 {
-    constexpr int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
-                              {1,4,7},{2,5,8},{3,6,9},
-                              {1,5,9},{3,5,7}};
+    static constexpr int ways[8][3] {{1,2,3},{4,5,6},{7,8,9},
+                                     {1,4,7},{2,5,8},{3,6,9},
+                                     {1,5,9},{3,5,7}};
     for (int i = 0; i != 8; ++i)
         if (s[ways[i][0]] == s[ways[i][1]] && s[ways[i][1]] == s[ways[i][2]])
             return 1;
